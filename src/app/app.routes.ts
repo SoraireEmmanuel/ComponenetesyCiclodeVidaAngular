1c6677a3abf98c15d9102469584e6e043db6a5f9
SoraireEmmanuel <59460182+SoraireEmmanuel@users.noreply.github.com>
2020-08-01 12:40:58 -0300

    optimizacion manejo de rutas hijas en archivos separados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,13 @@ import {  RouterModule, Routes } from '@angular/router'
 import { HomeComponent} from './componente/home/home.component'
 import { UsuarioComponent } from './componente/usuario/usuario.component'
 
-import { UsuarioNuevoComponent } from "./componente/usuario/usuario-nuevo.component";
-import { UsuarioEditarComponent}  from"./componente/usuario/usuario-editar.component";
-import { UsuarioDetalleComponent} from "./componente/usuario/usuario-detalle.component";
-
+import { USUARIO_ROUTES } from './componente/usuario/usuario.routes'
 
 const APP_ROUTES: Routes =[
     {path: 'home', component: HomeComponent},
     {path: 'usuario/:10', component:UsuarioComponent,
-    children:[
-        {path: 'nuevo', component: UsuarioNuevoComponent},
-        {path: 'editar', component: UsuarioEditarComponent},
-        {path: 'detalle', component: UsuarioDetalleComponent},
-        {path:'**', pathMatch:'full', redirectTo: 'nuevo'}
-    ] },
+    children:USUARIO_ROUTES
+},
     {path:'**', pathMatch:'full', redirectTo: 'home'}
 ];
 
